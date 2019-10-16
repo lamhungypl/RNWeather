a7d1e70075827f8f245c62afc6edf9d2f506c264
lamhung <lamhungypl@gmail.com>
2019-10-16 15:46:46 +0700

    first commit using create-react-native

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
